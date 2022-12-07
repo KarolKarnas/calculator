60b1e8627fd7da2d9cbc2151cae81104d5ad1b3e
KarolKarnas <karol.karnas@gmail.com>
2022-12-07 11:51:24 +0100

    Make buttons + - and to work
    add handles and event listeners to + - = buttons;
    create variable for second Number;
    add clearDisplay function after display firstVariable and after display the Result

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function add(a, b) {
+	// reset numbers
+	// firstNum = undefined;
+	// secondNum = undefined;
 	return a + b;
 }
 
@@ -15,8 +18,14 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-	return operator(a, b);
+	let result = operator(a, b);
+	populateDisplay(result);
+    clearAfterFirst = true;
+	console.log(`the result ${result}`);
+	return result;
 }
+
+// console.log(operate(add, 4, 5))
 
 // ----- DIGITS -----
 
@@ -37,68 +46,105 @@ const zero = document.querySelector('#zero');
 //function buttons
 const clear = document.querySelector('#clear');
 const addButton = document.querySelector('#addButton');
+const equalButton = document.querySelector('#equalButton');
+const subtractButton = document.querySelector('#subtractButton');
 
 // ---- LISTENERS ----
 //digits
 one.addEventListener('click', function () {
+	cleanAfterFirstNum();
 	populateDisplay(1);
 });
 two.addEventListener('click', function () {
+	cleanAfterFirstNum();
 	populateDisplay(2);
 });
 three.addEventListener('click', function () {
+	cleanAfterFirstNum();
 	populateDisplay(3);
 });
 four.addEventListener('click', function () {
+	cleanAfterFirstNum();
 	populateDisplay(4);
 });
 five.addEventListener('click', function () {
+	cleanAfterFirstNum();
 	populateDisplay(5);
 });
 six.addEventListener('click', function () {
+	cleanAfterFirstNum();
 	populateDisplay(6);
 });
 seven.addEventListener('click', function () {
+	cleanAfterFirstNum();
 	populateDisplay(7);
 });
 eight.addEventListener('click', function () {
+	cleanAfterFirstNum();
 	populateDisplay(8);
 });
 nine.addEventListener('click', function () {
+	cleanAfterFirstNum();
 	populateDisplay(9);
 });
 zero.addEventListener('click', function () {
+	cleanAfterFirstNum();
 	populateDisplay(0);
 });
 //function buttons
 clear.addEventListener('click', clearDisplay);
 addButton.addEventListener('click', storeAdd);
+subtractButton.addEventListener('click', storeSubtract);
+equalButton.addEventListener('click', getEquality);
 
 // ---- FUNCTIONS ----
 //store display value
 let displayValue;
 let firstNum;
+let secondNum;
 let operator;
+let clearAfterFirst;
 
 function populateDisplay(num) {
 	displayText.textContent += num;
 	displayValue = displayText.textContent;
-	console.log(displayValue);
+	console.log(`Display value: ${displayValue}`);
 }
 
 function clearDisplay() {
 	displayText.textContent = '';
 }
 
-function storeAdd() {
-firstNum = displayValue;
-operator = '+';
-console.log(firstNum, operator)
+function cleanAfterFirstNum() {
+	if (clearAfterFirst === true) {
+		clearDisplay();
+		clearAfterFirst = false;
+	}
 }
 
-// function storeOperator() {
+function storeAdd() {
+	firstNum = +displayValue;
+	operator = 'add';
+	clearAfterFirst = true;
+}
 
-// };
+function storeSubtract() {
+	firstNum = +displayValue;
+	operator = 'subtract';
+	clearAfterFirst = true;
+}
+
+function getEquality() {
+	secondNum = +displayValue;
+	clearDisplay();
+	if (operator === 'add') {
+        console.log(`operator: ${operator}, first number: ${firstNum}, second ${secondNum} `)
+		operate(add, firstNum, secondNum);
+	} else if (operator === 'subtract') {
+        console.log(`operator: ${operator}, first number: ${firstNum}, second ${secondNum} `)
+		operate(subtract, firstNum, secondNum);
+	}
+}
 
 // console.log(operate(add, 1, 2));
 // console.log(operate(subtract, 1, 2));
